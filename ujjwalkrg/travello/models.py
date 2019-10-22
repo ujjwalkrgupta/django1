82977439c5544f83ea9c7475d9b020c4aef90d4b
ujjwalkrgupta <35277607+ujjwalkrgupta@users.noreply.github.com>
2019-10-23 04:26:14 +0530

    commit 3

--- ujjwalkrg/travello/models.py
+++ ujjwalkrg/travello/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class destination:
+    id : int
+    name : str
+    image : str
+    price : int
+    desc : str
--- ujjwalkrg/travello/views.py
+++ ujjwalkrg/travello/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import destination
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+
+    dest1 = destination()
+    dest1.name = 'mumbai'
+    dest1.price = 700
+    dest1.desc = 'city that never sleeps'
+    
+    return render(request, 'index.html',{'dest1':dest1})
