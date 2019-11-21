59aca17ea7f4a543d5770598cef4f271be654ab2
ujjwalkrgupta <35277607+ujjwalkrgupta@users.noreply.github.com>
2019-11-21 19:01:12 +0530

    new commit

--- ujjwalkrg/travello/views.py
+++ ujjwalkrg/travello/views.py
@@ -8,6 +8,22 @@ def index(request):
     dest1 = destination()
     dest1.name = 'mumbai'
     dest1.price = 700
+    dest1.img = 'destination_1.jpg'
     dest1.desc = 'city that never sleeps'
+
+    dest2 = destination()
+    dest2.name = 'kolkata'
+    dest2.img = 'destination_2.jpg'
+    dest2.price = 800
+    dest2.desc = 'city of joy'
+
+    dest3 = destination()
+    dest3.name = 'bangalore'
+    dest3.img = 'destination_3.jpg'
+    dest3.price = 900
+    dest3.desc = 'silicon valley of india'
+
+    dests =[dest1,dest2,dest3]
+
     
-    return render(request, 'index.html',{'dest1':dest1})
+    return render(request, 'index.html',{'dests':dests})
