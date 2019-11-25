3bfd9f8b93359fcb58921f2ea6e25852f66cffb9
ujjwalkrgupta <35277607+ujjwalkrgupta@users.noreply.github.com>
2019-11-25 19:52:04 +0530

    migrations

--- ujjwalkrg/travello/models.py
+++ ujjwalkrg/travello/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class destination:
-    id : int
-    name : str
-    image : str
-    price : int
-    desc : str
+class destination(models.Model):
+    
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='pics')
+    price = models.IntegerField()
+    desc = models.TextField()
+    offer = models.BooleanField(default=False)
